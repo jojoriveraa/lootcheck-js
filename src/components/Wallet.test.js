d1f9c99d6dc929d87770992d52222fad1b7f6513
Jorge Rivera <jariverai@gmail.com>
2019-09-02 14:58:15 -0500

    [bdd] wallet-component
    * Delete .jshintrc
    
    * Update index.js
    
    * Create Wallet.test.js
    
    * Update Wallet.test.js: Wallet renders properly
    
    * [feature-bdd] wallet-component
    
    * Create Wallet.js
    
    * ci-cd: optimise workflows in github actions (#63)
    
    * ci-cd: optimise workflows in github actions
    
    * Split github actions workflows
    
    * Update workflow.yml
    
    * Update workflow.yml
    
    * Update workflow.yml

---
+++ src/components/Wallet.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Wallet from './Wallet';
+
+describe('Wallet', () => {
+  const wallet = shallow(<Wallet />);
+  it('renders properly', () => {
+    expect(wallet).toMatchSnapshot();
+  });
+});
