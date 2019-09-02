62cc0ce85e5b4256ca15fe7bef3ceaffb74c59e5
Jorge Rivera <jariverai@gmail.com>
2019-09-02 14:56:28 -0500

    [feature-bdd] wallet-component
    * Create Wallet.js
    
    * ci-cd: optimise workflows in github actions (#63)
    
    * ci-cd: optimise workflows in github actions
    
    * Split github actions workflows
    
    * Update workflow.yml
    
    * Update workflow.yml
    
    * Update workflow.yml

---
+++ src/components/Wallet.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Wallet extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Wallet Balance</h3>
+      </div>
+    );
+  }
+}
+
+export default Wallet;
