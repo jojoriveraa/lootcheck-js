f10a71ff5a3950bd5db9907ca9cdaccc19e46dfb
Jorge Rivera <jariverai@gmail.com>
2019-09-03 14:51:51 -0500

    [Refator] Reoganize imports and format

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { createStore } from 'redux';
-import rootReducer from './reducers/balance';
 import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import App from './components/App';
+import rootReducer from './reducers/balance';
 
 const store = createStore(rootReducer);
 
