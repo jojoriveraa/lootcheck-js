bf762f1de1c25e5c8b7ffcb1e340d7c7322efb6d
Jorge Rivera <jariverai@gmail.com>
2019-09-03 18:21:27 -0500

    [Test] update local wallet on input

--- src/components/Wallet.test.js
+++ src/components/Wallet.test.js
@@ -19,4 +19,18 @@ describe('Wallet', () => {
   it('creates an input to deposit or withdraw from the balance', () => {
     expect(wallet.find('.input-wallet').exists()).toBe(true);
   });
+
+  describe('when the user types into the wallet input', () => {
+    const userBalance = '25';
+
+    beforeEach(() => {
+      wallet
+        .find('.input-wallet')
+        .simulate('change', { target: { value: userBalance } });
+    });
+
+    it('updates the local wallet balance in `state` and converts that into a number', () => {
+      expect(wallet.state().balance).toEqual(parseInt(userBalance));
+    });
+  });
 });
