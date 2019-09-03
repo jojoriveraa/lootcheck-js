0fc0dffaeaa4e1a080799651305a97be4ea3bc1b
Jorge Rivera <jariverai@gmail.com>
2019-09-03 18:35:18 -0500

    [Feature] Update local wallet balance with input value

--- src/components/Wallet.js
+++ src/components/Wallet.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+import balanceReducer from '../reducers/balance';
+import * as constants from '../actions/constants';
+
 export class Wallet extends Component {
+  constructor() {
+    super();
+    this.state = { balance: undefined };
+  }
+
+  updateBalance = event =>
+    this.setState({ balance: parseInt(event.target.value) });
+
   render() {
     return (
       <div>
         <h3 className="balance">Wallet Balance: {this.props.balance}</h3>
         <br />
-        <input className="input-wallet"></input>
+        <input className="input-wallet" onChange={this.updateBalance}></input>
       </div>
     );
   }
