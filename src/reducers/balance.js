0dedf9168bcdefaedbd139bf47f02e96d423badd
Jorge Rivera <jariverai@gmail.com>
2019-09-03 15:54:00 -0500

    [Feature] Withdraws from the balance

--- src/reducers/balance.js
+++ src/reducers/balance.js
@@ -6,6 +6,8 @@ const balance = (state = 0, action) => {
       return action.balance;
     case constants.DEPOSIT:
       return state + action.deposit;
+    case constants.WITHDRAW:
+      return state - action.withdraw;
     default:
       return state;
   }
