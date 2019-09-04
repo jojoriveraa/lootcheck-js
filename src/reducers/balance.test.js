1931013052f107c9eb05bce667f90aa087edf066
Jorge Rivera <jariverai@gmail.com>
2019-09-04 10:40:29 -0500

    [Test] Group tests of "Initialize" scenario

--- src/reducers/balance.test.js
+++ src/reducers/balance.test.js
@@ -3,22 +3,23 @@ import balanceReducer2 from './balance';
 import * as constants from '../actions/constants';
 
 describe('balanceReducer', () => {
-  const balance = 10;
+  describe('when initializing', () => {
+    it('sets a balance', () => {
+      expect(
+        balanceReducer(undefined, {
+          type: constants.SET_BALANCE,
+          balance
+        })
+      ).toEqual(balance);
+    });
 
-  it('sets a balance', () => {
-    expect(
-      balanceReducer(undefined, {
-        type: constants.SET_BALANCE,
-        balance
-      })
-    ).toEqual(balance);
-  });
-
-  describe('then re-initializing', () => {
-    it('reads the balance from cookies', () => {
-      expect(balanceReducer2(undefined, {})).toEqual(balance);
+    describe('then re-initializing', () => {
+      it('reads the balance from cookies', () => {
+        expect(balanceReducer2(undefined, {})).toEqual(balance);
+      });
     });
   });
+  const balance = 10;
 
   it('deposits into the balance', () => {
     const deposit = 10;
