66e81f74c7cfcd9e3618e0d1f1bb59d1ca632fb0
Jorge Rivera <jariverai@gmail.com>
2019-09-04 10:38:01 -0500

    [Test] Reads the balance from cookies

--- src/reducers/balance.test.js
+++ src/reducers/balance.test.js
@@ -1,15 +1,23 @@
 import balanceReducer from './balance';
+import balanceReducer2 from './balance';
 import * as constants from '../actions/constants';
 
 describe('balanceReducer', () => {
+  const balance = 10;
+
   it('sets a balance', () => {
-    const balance = 10;
     expect(
       balanceReducer(undefined, {
         type: constants.SET_BALANCE,
         balance
       })
     ).toEqual(balance);
+  });
+
+  describe('then re-initializing', () => {
+    it('reads the balance from cookies', () => {
+      expect(balanceReducer2(undefined, {})).toEqual(balance);
+    });
   });
 
   it('deposits into the balance', () => {
