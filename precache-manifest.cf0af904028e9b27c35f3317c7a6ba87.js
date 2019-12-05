5d5becddf08c360b581df6ce7040c65e71a23124
jojoriveraa <jojoriveraa@users.noreply.github.com>
2019-12-05 13:52:40 +0000

    Automated deployment: Thu Dec  5 13:52:40 UTC 2019 37170b592ab36d910def35df1c76aab601591ade

---
+++ precache-manifest.cf0af904028e9b27c35f3317c7a6ba87.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5de54eff009a5fa577e59a9327512696",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5815f625426fbb91e80e",
+    "url": "/static/js/2.6fa5a70e.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.6fa5a70e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7d29702aba8862d822f1",
+    "url": "/static/js/main.91dfb254.chunk.js"
+  },
+  {
+    "revision": "a6248218c8a9d0ed90b2",
+    "url": "/static/js/runtime-main.1356c149.js"
+  }
+]);
